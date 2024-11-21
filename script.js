e61d6b9c897055d4c18361fe46551978b2884239
buffalojoe <42217874+buffalojoe@users.noreply.github.com>
2024-11-21 08:49:56 -0500

    Added event listeners to JS file and improved calculator UI

--- script.js
+++ script.js
@@ -30,3 +30,101 @@ function operate(operator, num1, num2) {
             break;
     };
 }
+
+
+let displayValue = document.querySelector(".display p");
+let number1 = undefined;
+let number2 = undefined;
+
+function checkForValidFirstNumber(number, operator) {
+
+    console.log(number);
+
+    let numberCheck = Number(number);
+
+}
+
+function clearContent() {
+    displayValue.textContent = '';
+}
+
+function checkInvalid() {
+
+    
+    if (displayValue.textContent == "INVALID") {
+        clearContent();
+    } else {
+        return;
+    }
+}
+
+function onlyDigits(str) {
+    return /^\d+$/.test(str);
+}
+
+num1.addEventListener("click", (event) => {
+    checkInvalid();
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num2.addEventListener("click", (event) => {
+    checkInvalid();
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num3.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num4.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num5.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num6.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num7.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num8.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num9.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+num0.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+
+addition.addEventListener("click", (event) => {
+    checkInvalid();
+    checkForValidFirstNumber(displayValue.textContent, event.target.textContent);
+    
+})
+subtraction.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+multiplication.addEventListener("click", (event) => {
+    checkInvalid();
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+division.addEventListener("click", (event) => {
+    checkInvalid();   
+    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
+})
+
+equals.addEventListener("click", (event) => {   
+    checkInvalid();
+})
+
+clear.addEventListener("click", (event) => {   
+    displayValue.textContent = '';
+})
