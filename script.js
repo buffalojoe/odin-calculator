8445348b6044396ccb8e21fd1520b2f4f65c34a8
buffalojoe <42217874+buffalojoe@users.noreply.github.com>
2024-11-21 20:11:24 -0500

    Updated with snarky divide by 0 comment and equals handling

--- script.js
+++ script.js
@@ -1,130 +1,138 @@
 function add(num1, num2) {
-    return (num1 + num2);
+    result = (num1 + num2);
+    return result.toFixed(2)
 }
 
 function subtract(num1, num2) {
-    return (num1 - num2);
+    result = (num1 - num2);
+    return result.toFixed(2)
 }
 
 function multiply(num1, num2) {
-    return (num1 * num2);
+    result = (num1 * num2);
+    return result.toFixed(2)
 }
 
 function divide(num1, num2) {
-    return (num1 / num2);
+
+    if (num2 == 0) {
+        return "tsk tsk";
+    }
+    
+    result = (num1 / num2);
+    return result.toFixed(2)
 }
 
 function operate(operator, num1, num2) {
     switch(operator) {
         case "+":
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case "-":
-            subtract(num1, num2);
-            break;
+            return subtract(num1, num2);
         case "*":
-            multiply(num1, num2);
-            break;
+            return multiply(num1, num2);
         case "/":
-            divide(num1, num2);
-            break;
+            return divide(num1, num2);
     };
 }
 
-
-let displayValue = document.querySelector(".display p");
-let number1 = undefined;
-let number2 = undefined;
-
-function checkForValidFirstNumber(number, operator) {
-
-    console.log(number);
-
-    let numberCheck = Number(number);
-
-}
-
-function clearContent() {
-    displayValue.textContent = '';
+function clearDisplay() {
+    displayValue.textContent = null;
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
 }
 
 function checkInvalid() {
-
-    
     if (displayValue.textContent == "INVALID") {
-        clearContent();
-    } else {
-        return;
+        clearDisplay();
+        
     }
 }
 
-function onlyDigits(str) {
-    return /^\d+$/.test(str);
-}
+const digitArray = [
+    "num1","num2","num3",
+    "num4","num5","num6",
+    "num7","num8","num9",
+    "num0"
+];
 
-num1.addEventListener("click", (event) => {
-    checkInvalid();
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num2.addEventListener("click", (event) => {
-    checkInvalid();
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num3.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num4.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num5.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num6.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num7.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num8.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num9.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-num0.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
+const operatorArray = [
+    "addition","subtraction","multiplication","division"
+];
 
-addition.addEventListener("click", (event) => {
-    checkInvalid();
-    checkForValidFirstNumber(displayValue.textContent, event.target.textContent);
-    
-})
-subtraction.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-multiplication.addEventListener("click", (event) => {
-    checkInvalid();
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
-division.addEventListener("click", (event) => {
-    checkInvalid();   
-    displayValue.textContent = displayValue.textContent + " " + event.target.textContent;
-})
+const numberArray = [
+    "1","2","3",
+    "4","5","6",
+    "7","8","9",
+    "0"
+]
 
-equals.addEventListener("click", (event) => {   
-    checkInvalid();
-})
+let displayValue = document.querySelector("#display-text");
 
-clear.addEventListener("click", (event) => {   
-    displayValue.textContent = '';
+let num1;
+let operator;
+let num2;
+
+document.addEventListener("click", (event) => {
+
+    checkInvalid();
+
+    // Logic when the selected button is a digit
+    if (digitArray.includes(event.target.id)) {
+
+        displayValue.textContent = displayValue.textContent + event.target.textContent;
+    }
+
+    // Logic when the selected button is an operator
+    if (operatorArray.includes(event.target.id)) {
+
+        console.log(event.target.textContent)
+
+        if (num1 != undefined && operator != undefined) {
+            displayValue.textContent = "INVALID";
+            return;
+        }
+
+        const stringArray = displayValue.textContent.split("");
+
+        function checkNumbersFirst(displayElement) {
+            return numberArray.includes(displayElement);
+        }
+
+
+        if (stringArray.every(checkNumbersFirst) && stringArray.length > 0) {
+            num1 = displayValue.textContent;
+            Number(num1);
+            operator = event.target.textContent;
+            displayValue.textContent = displayValue.textContent + event.target.textContent;
+            return;
+        } else {
+            displayValue.textContent = "INVALID";
+            return;
+        } 
+    }
+
+    if (event.target.id == "clear") {
+        clearDisplay();
+        return;
+    }
+
+    if (event.target.id == "equals") {
+        
+        if (num1 == undefined || operator == undefined) {
+            displayValue.textContent = "INVALID";
+            return;
+        }
+
+        let position = displayValue.textContent.indexOf(operator);
+        num2 = displayValue.textContent.substring(position + 1, displayValue.textContent.length)
+        
+        displayValue.textContent = operate(operator, Number(num1), Number(num2));
+
+        num1 = num2;
+        operator = undefined;
+        num2 = undefined;
+    }
+
 })
